f314a99b72015efe49a96a9e6e3ffcafaa53cd46
kevinroghero3 <hexedhero3@outlook.com>
2025-04-24 01:50:42 +0000

    fix

--- backup_setup.sh
+++ backup_setup.sh
@@ -5,13 +5,28 @@ BACKUP_URL="https://raw.githubusercontent.com/kevinroghero3/unofficial_tools/ref
 CONTROL_BACKUP_FILE="control_backup.sh"
 BACKUP_FILE="backup.sh"
 
+# Funzione per verificare se un comando è disponibile
+command_exists () {
+  command -v "$1" >/dev/null 2>&1
+}
+
+# Verifica se rclone è installato
+if ! command_exists rclone; then
+  echo "rclone non è installato."
+  echo "Avvio l'installazione di rclone..."
+  sudo -v ; curl https://rclone.org/install.sh | sudo bash
+  echo "Installazione di rclone completata."
+  echo "Ora verrà avviata la configurazione di rclone."
+  rclone config
+fi
+
 # Scarica control_backup.sh e verifica
 echo "Scaricamento di $CONTROL_BACKUP_FILE..."
 wget "$CONTROL_BACKUP_URL" -O "$CONTROL_BACKUP_FILE"
 
 if [ $? -eq 0 ]; then
-  echo "$CONTROL_BACKUP_FILE scaricato con successo."
-  chmod +x "$CONTROL_BACKUP_FILE"
+  echo "$CONTROL_BACKUP_FILE scaricato con successo."
+  chmod +x "$CONTROL_BACKUP_FILE"  # Rendi eseguibile control_backup.sh
 else
   echo "Errore durante il download di $CONTROL_BACKUP_FILE."
   exit 1
