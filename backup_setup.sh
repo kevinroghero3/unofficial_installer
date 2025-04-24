27c88c86d2fb6f4101d3beb97ce74f8c6f388a0c
kevinroghero3 <hexedhero3@outlook.com>
2025-04-24 01:44:37 +0000

    Revert "Update backup_setup.sh"
    This reverts commit db72e1ee82c16808c20b34f4ad5c22fdaf442e5a.

--- backup_setup.sh
+++ backup_setup.sh
@@ -5,31 +5,16 @@ BACKUP_URL="https://raw.githubusercontent.com/kevinroghero3/unofficial_tools/ref
 CONTROL_BACKUP_FILE="control_backup.sh"
 BACKUP_FILE="backup.sh"
 
-# Funzione per verificare se un comando è disponibile
-command_exists () {
-  command -v "$1" >/dev/null 2>&1
-}
-
-# Verifica se rclone è installato
-if ! command_exists rclone; then
-  echo "rclone non è installato."
-  echo "Avvio l'installazione di rclone..."
-  sudo -v ; curl https://rclone.org/install.sh | sudo bash
-  echo "Installazione di rclone completata."
-  echo "Ora verrà avviata la configurazione di rclone."
-  rclone config
-fi
-
 # Scarica control_backup.sh e verifica
 echo "Scaricamento di $CONTROL_BACKUP_FILE..."
 wget "$CONTROL_BACKUP_URL" -O "$CONTROL_BACKUP_FILE"
 
 if [ $? -eq 0 ]; then
   echo "$CONTROL_BACKUP_FILE scaricato con successo."
-  chmod +x "$CONTROL_BACKUP_FILE"  # Rendi eseguibile control_backup.sh
+  chmod +x "$CONTROL_BACKUP_FILE"
 else
-  echo "Errore durante il download di $CONTROL_BACKUP_FILE."
-  exit 1
+  echo "Errore durante il download di $CONTROL_BACKUP_FILE."
+  exit 1
 fi
 
 # Scarica backup.sh e verifica
@@ -37,11 +22,11 @@ echo "Scaricamento di $BACKUP_FILE..."
 wget "$BACKUP_URL" -O "$BACKUP_FILE"
 
 if [ $? -eq 0 ]; then
-  echo "$BACKUP_FILE scaricato con successo."
-  chmod +x "$BACKUP_FILE"
+  echo "$BACKUP_FILE scaricato con successo."
+  chmod +x "$BACKUP_FILE"
 else
-  echo "Errore durante il download di $BACKUP_FILE."
-  exit 1
+  echo "Errore durante il download di $BACKUP_FILE."
+  exit 1
 fi
 
 # Esegui control_backup.sh per avviare il cron job
